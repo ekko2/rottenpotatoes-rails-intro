c6999d45567aad20d979481bb52aa7e6526511c9
EC2 Default User <ec2-user@ip-172-31-8-26.us-east-2.compute.internal>
2019-02-11 20:47:46 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
-  
+
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
   def show
@@ -11,26 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort = params[:sort] || session[:sort]
-    case sort
-    when 'title'
-      ordering,@title_header = {:title => :asc}, 'hilite'
-    when 'release_date'
-      ordering,@date_header = {:release_date => :asc}, 'hilite'
-    end
-    @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || session[:ratings] || {}
-    
-    if @selected_ratings == {}
-      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
-    end
-    
-    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
-      session[:sort] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
-    end
-    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+    @movies = Movie.all
   end
 
   def new
@@ -61,12 +42,4 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-  def find_similar_movies
-    @movies = Movie.similar_movies(params[:id].to_i)
-    @movie = Movie.find(params[:id])
-      if @movies.nil? or @movie.director.empty?
-        flash[:warning ] = "'#{@movie.title}' has no director info" 
-      redirect_to root_path
-      end
-  end
 end
